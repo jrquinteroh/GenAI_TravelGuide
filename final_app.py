22b368a11cd88a6e2300b6803128e66790694428
Juan Quintero <167062240+jrquinteroh@users.noreply.github.com>
2024-11-26 20:42:51 +0100

    Update final_app.py

--- final_app.py
+++ final_app.py
@@ -397,7 +397,7 @@ with tab_input:
         st.markdown("[🚲 Click here to view bicycle routes!](https://www.example.com/car-rental)")
 
     if 'Walking' in transportation:
-        st.markdown("[🚶‍♂️‍➡️🚶‍♀️ Click here to view transport via boat (fancy😎💰)!](https://www.example.com/car-rental)")
+        st.markdown("[🚶‍♂️‍➡️🚶‍♀️ Click here to walking routes!](https://www.example.com/car-rental)")
     
     interests = st.text_input("Hobbies/Interests (separate by commas)")
     budget_per_person = st.number_input("Budget per Person per Day ($)", min_value=0.0, step=10.0)
